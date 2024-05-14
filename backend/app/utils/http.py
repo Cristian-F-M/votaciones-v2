e01f5f6306467209e74a7e9f2414cc734b2dc8a3
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 21:44:10 -0500

    feat: Add accepted methods to HTTP utils

--- backend/app/utils/http.py
+++ backend/app/utils/http.py
@@ -1,7 +1,7 @@
 import json
 
 ACCEPTED_ORIGINS = ["http://localhost:8080", "http://localhost:5173"]
-
+ACCEPTED_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 
 def json_response(object):
     return json.dumps(object)
