400dc395ab0ec1618ec1d6c94e5aff530a7ccfc5
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 20:42:09 -0500

    refactor: Update main.py to use JSON response helper functions

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,13 +1,27 @@
 from flask import Blueprint
 from flask import request, Response
-import json
+from app.utils.http import json_response, middlware_after_request
+from app.models.User import User
+
 
 bp = Blueprint("main", __name__)
 
 
-@bp.route("/data")
-def index():
-    data = {"data": {"more_data": 1}}
-    response = Response(json.dumps(data), status=200, mimetype='application/json')
-    response.headers['Access-Control-Allow-Origin'] = '*'
+
+@bp.before_request
+def before_request():
+    middlware_after_request(request)
+
+@bp.route("/test", methods=["GET"])
+def test():
+    data = json_response({ "res": "All ok" })
+    response = Response(data, status=200, mimetype='application/json')
     return response
+
+
+
+
+@bp.route("/user/register", methods=["POST"])
+def user_register():
+    data = request.json
+    return Response(json_response(data), status=200, mimetype='application/json')
