a85afc76eab92a54f1148ebd5ec3de17f766a2c8
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 21:07:34 -0500

    refactor: Remove unused middleware

--- backend/app/utils/http.py
+++ backend/app/utils/http.py
@@ -1,15 +1,7 @@
 import json
-from flask import request
 
 ACCEPTED_ORIGINS = ["http://localhost:8080", "http://localhost:5173"]
 
 
 def json_response(object):
     return json.dumps(object)
-
-
-def middlware_after_request(response):
-    origin = request.origin
-    if origin in ACCEPTED_ORIGINS:
-        response.headers['Access-Control-Allow-Origin'] = origin
-    return response
