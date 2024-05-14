192fd631bb5a023534638b398e0a41ca423476ee
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-14 13:31:06 -0500

    refactor: Initialize Flask Bcrypt extension in __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("Config.Config")
 
     db.init_app(app)
-
+    bcrypt.init_app(app)    
 
     from app.routes import main
     app.register_blueprint(main.bp)
