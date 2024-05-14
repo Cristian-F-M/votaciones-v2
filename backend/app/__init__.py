9c3473f42cc1f7b498a02d454ab06c294e502a71
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 20:41:30 -0500

    refactor: Remove unuse code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-import uuid
 
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.urandom(24)
     app.config.from_object("Config.Config")
 
     db.init_app(app)
