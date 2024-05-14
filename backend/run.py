6804a5a6adcc19e99650b406c4e33b27e53ce1e7
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-14 13:30:48 -0500

    refactor: Add seed_database function to run.py

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,6 @@
 from app import create_app, db
 from dotenv import load_dotenv
+from app.utils.seed import seed_database
 
 load_dotenv()
 
@@ -7,6 +8,7 @@ app = create_app()
 
 with app.app_context():
     db.create_all()
+    seed_database()
 
 if __name__ == "__main__":
     app.run()
