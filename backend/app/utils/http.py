e900511a1e65b93098d3982258a7fd20afc250f4
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 20:41:55 -0500

    feat: Add CORS middleware for handling accepted origins

--- backend/app/utils/http.py
+++ backend/app/utils/http.py
@@ -1,4 +1,15 @@
 import json
+from flask import request
 
-def json(object):
+ACCEPTED_ORIGINS = ["http://localhost:8080", "http://localhost:5173"]
+
+
+def json_response(object):
     return json.dumps(object)
+
+
+def middlware_after_request(response):
+    origin = request.origin
+    if origin in ACCEPTED_ORIGINS:
+        response.headers['Access-Control-Allow-Origin'] = origin
+    return response
