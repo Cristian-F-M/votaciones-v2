22c50e5bc928b812a8e3f79d2000233ef52b9ccb
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-15 16:19:15 -0500

    refactor: Add Session model for user sessions

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.Candidate import Candidate
 from app.models.Type_document import Type_document
 from app.models.Role import Role
 from app.models.User import User
+from app.models.Session import Session
