836a7ed3a7acf838c0071c869fd5bc22d7654b41
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-15 16:20:22 -0500

    refactor: Add constant for session expiration time

--- backend/app/utils/http.py
+++ backend/app/utils/http.py
@@ -2,6 +2,7 @@ import json
 
 ACCEPTED_ORIGINS = ["http://localhost:8080", "http://localhost:5173"]
 ACCEPTED_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+TIME_EXPIRATION_SESSION = 60 * 60 * 4 # 4 hours  
 
 def json_response(object):
     return json.dumps(object)
