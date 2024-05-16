e1d0ba406f967d1f9315ea962abff5c72b75038d
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-16 14:24:12 -0500

    refactor: Add tables for roles and types of documents

---
+++ backend/migrations/versions/b7c5afccd406_seed_data.py
@@ -0,0 +1,24 @@
+"""Seed data
+
+Revision ID: b7c5afccd406
+Revises: 
+Create Date: 2024-05-16 13:10:09.232600
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b7c5afccd406'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
