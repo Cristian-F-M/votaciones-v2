c0c860c608728726973621727fc23289b96d0b94
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-16 12:10:00 -0500

    refactor: Update description column length to 120 characters

--- backend/app/models/Type_document.py
+++ backend/app/models/Type_document.py
@@ -4,7 +4,7 @@ class Type_document(db.Model):
     __tablename__ = 'types_document'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
-    description = db.Column(db.String(50))
+    description = db.Column(db.String(120))
 
     def to_dict(self):
         type_document = {
