36ff7bc030c39fa6ba80e8f0d7aaa5920a8e82ec
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-20 13:02:16 -0500

    feat: Update App component to use react-router-dom for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
+// import { BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, NavLink, Link, Routes } from "react-router-dom";
+import { About } from "@/pages/About";
+import { Home } from "@/pages/Home";
+import { Login } from "@/pages/Login";
+import { Register } from "@/pages/Register";
+import { NotFound } from "@/pages/404";
+import { Header } from "@/components/Header";
+
 function App() {
-
-
-
     return (
         <>
-            <div>
-            </div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/Register" element={<Register />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
