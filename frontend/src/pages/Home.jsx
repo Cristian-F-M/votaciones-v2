14dd9d8f59f210c07dd1a66404b6369ab9d54473
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-20 13:00:51 -0500

    feat: Add Home page component with login requirement and dynamic list rendering

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { getApi, loginRequired } from "@/utils/API";
+
+export function Home() {
+
+    loginRequired()
+
+    return (
+    <>
+        <h1>Home</h1>
+
+        {
+            a.map(b => {
+                return <li>{b}</li>
+            })
+        }
+    </>
+    );
+}
