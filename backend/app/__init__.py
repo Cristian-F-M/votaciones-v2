3e878dd9b1e25356c8a0ad357ce565050d5defe8
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-23 12:09:57 -0500

    chore: Add CORS configuration in __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_cors import CORS
+from env import ACEPTED_ORIGIN
+
+print(ACEPTED_ORIGIN)
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -14,6 +18,11 @@ def create_app():
     bcrypt.init_app(app)    
     Migrate(app, db)
 
+    CORS(app, supports_credentials=True, resources={
+        r"/*": {
+            "origins": [ACEPTED_ORIGIN]
+        }
+    })
 
     from app.routes import main, user, resources
     app.register_blueprint(main.bp)
